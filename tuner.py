0ea035ff2f35d5e67776220fa1af16e1de4e119d
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-05-03 15:11:12 +0530

    added a flask server

--- tuner.py
+++ tuner.py
@@ -1,41 +1,20 @@
 import sys
+import os
 
-param_count = len(sys.argv[1:])
+from flask import Flask
+from flask import request
 
-assert param_count%2==0
+app = Flask(__name__)
 
-config_file = open("/etc/apache2/mods-available/mpm_prefork.conf").read()
-# config_file = open("mpm_prefork.conf").read()
+@app.route("/setParam")
+def getParam():
+    config_file = open("/etc/apache2/mods-available/mpm_prefork.conf").read()
+    config_file = config_file.replace("{{MaxRequestWorkers}}", request.args.get("MaxRequestWorkers"))
 
+    with open("/etc/apache2/mods-available/mpm_prefork.conf", "w")as f:
+        f.write(config_file)
 
-# params = {
-#     "MinSpareServers" : 5,
-#     "MaxSpareServers" : 10,
-#     "MaxRequestWorkers" : 256
-# }
+    os.system("sudo /etc/init.d/apache2 reload")
+    return True
 
-params = {
-    "MaxRequestWorkers" : 256
-}
-
-
-for i in xrange(0, param_count, 2):
-    params[sys.argv[i+1]] = str(sys.argv[i+2])
-
-for param in params:
-    config_file = config_file.replace("{{" + param + "}}", params[param])
-
-# with open("test.conf", "w")as f:
-#     f.write(config_file)
-
-with open("/etc/apache2/mods-available/mpm_prefork.conf", "w")as f:
-    f.write(config_file)
-
-# gracefully restart the apache server
-import subprocess
-
-process = subprocess.Popen("sudo /etc/init.d/apache2 stop", stdout=subprocess.PIPE)
-output, error = process.communicate()
-
-process = subprocess.Popen("sudo /etc/init.d/apache2 start", stdout=subprocess.PIPE)
-output, error = process.communicate()
+app.run(port=5001)
