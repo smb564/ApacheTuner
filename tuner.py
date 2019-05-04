dc621ef9369fd5326bda6151a2f6dc0e9c1dd2ef
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-05-04 13:18:47 +0530

    fixed issue

--- tuner.py
+++ tuner.py
@@ -6,13 +6,14 @@ from flask import request
 
 app = Flask(__name__)
 
-config_file = open("mpm_prefork.conf").read()
+with open("mpm_prefork.conf") as f:
+    config_file = f.read()
 
 @app.route("/setParam")
 def getParam():
     config_file_updated = config_file.replace("{{MaxRequestWorkers}}", request.args.get("MaxRequestWorkers"))
 
-    with open("/etc/apache2/mods-available/mpm_prefork.conf", "w")as f:
+    with open("/etc/apache2/mods-available/mpm_prefork.conf", "w") as f:
         f.write(config_file_updated)
 
     os.system("sudo /etc/init.d/apache2 reload")
