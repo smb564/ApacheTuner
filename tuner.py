faa9cf49c845a3f80156d2dc9884acb4cf38ff0e
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-05-05 21:05:47 +0530

    fixed bug : accesing mpmModel before def

--- tuner.py
+++ tuner.py
@@ -6,17 +6,12 @@ from flask import request
 
 app = Flask(__name__)
 
-# TODO: We should read the current modul from mods-available
-# let's always put mpm_event as the initial one
-model = ""
-mpmModel("mpm_event")
-
 
 @app.route("/setParam")
 def getParam():
     with open(model + ".conf") as f:
         config_file = f.read()
-        
+
     config_file = config_file.replace("{{MaxRequestWorkers}}", request.args.get("MaxRequestWorkers"))
 
     with open("/etc/apache2/mods-available/" + model + ".conf", "w") as f:
@@ -25,9 +20,11 @@ def getParam():
     os.system("sudo /etc/init.d/apache2 reload")
     return "Done"
 
+
 @app.route("/setModel")
 def setModel():
     return mpmModel(request.args.get("model"))
+
 
 def mpmModel(new_model):
     global model
@@ -56,10 +53,15 @@ def setDefault():
     os.system("cp " + model + ".default.conf /etc/apache2/mods-available/" + model + ".conf")
     return "Done"
 
-    
 
 @app.route("/")
 def hello():
     return "Hello, Server is runing.."
+
+
+# TODO: We should read the current modul from mods-available
+# let's always put mpm_event as the initial one
+model = ""
+mpmModel("mpm_event")
 
 app.run(host="0.0.0.0", port=5001)
